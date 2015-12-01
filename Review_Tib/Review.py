e5bd33e55c04974625a8fc7cbab8ccd2cbf5e637
bitakou <bitakou555@gmail.com>
2015-12-01 16:44:41 +0700

    Review class

---
+++ Review_Tib/Review.py
@@ -0,0 +1,8 @@
+class Review(object):
+    
+    def __init__(self, username, description, rating, suggested_route, upload_photo)
+        self.username = username
+        self.description = description
+        self.rating = rating
+        self.suggested_route = suggested_route
+        self.upload_photo = upload_photo
